054f13281f69ccf6d07975fde76feb2085ef052b
Nishant Kohli <nishkohli96@gmail.com>
2021-06-23 19:49:29 +0530

    react-app-rewired setup

---
+++ packages/frontend/config-overrides.js
@@ -0,0 +1,13 @@
+const { alias } = require('react-app-rewire-alias');
+
+const aliasMap = {
+    _Images: 'src/assets/images',
+    _Pages: 'src/pages',
+    _Shared: 'src/shared',
+    _Styles: 'src/assets/styles',
+    _Utils: 'src/utils',
+};
+
+module.exports = alias(aliasMap);
+
+/* For more info- https://www.npmjs.com/package/react-app-rewire-alias */
